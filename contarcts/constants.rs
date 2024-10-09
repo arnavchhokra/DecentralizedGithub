1b22a66396496c842ee9cdcdcd5db74a9764dc72
Arnav Chhokra <arnavchhokra@gmail.com>
2024-10-09 11:40:53 +0530

    commit

---
+++ contarcts/constants.rs
@@ -0,0 +1,4 @@
+pub const USER_SEED: &[u8] = b"user";
+pub const REPO_SEED: &[u8] = b"repo";
+pub const PULL_SEED: &[u8] = b"pull";
+// pub const GLOBAL_STATE_SEED: &[u8] = b"global_state"; // Example seed
